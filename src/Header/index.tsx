3d49bfad1a3b9c61f9a1adda084942ddbed70ccf
Phan Đình Huy <kompadev@KompaDevs-MacBook-Pro.local>
2021-10-21 15:27:45 +0700

    update

---
+++ src/Header/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Image from "../components/Image";
+import useMatchLocation from "../hooks/useMatchLocation";
+import DesktopMenu from "./DesktopMenu";
+import Search from "./Search";
+
+const Header = () => {
+  const matchedRoute = useMatchLocation();
+
+  return (
+    <div className="fixed z-30 px-4 flex justify-center items-center w-screen h-16 bg-background-lighter">
+      <div className="absolute left-0 p-in">
+        <Link
+        
+        style={{
+          fontWeight: "bold",
+          fontSize: "30",
+          color: "red",
+        }}
+        to="/">
+          HUYANIME
+          {/* <Image src="/logo.png" alt="logo" /> */}
+        </Link>
+      </div>
+
+      <div className="md:hidden absolute right-0 p-in flex justify-center items-center"></div>
+
+      <DesktopMenu matchedRoute={matchedRoute} />
+
+      <div className="hisdden md:block absolute right-0 p-in">
+        <Search />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
